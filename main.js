04375b51d8acf2842d40f4da91c076a5e237a721
Fdg1994 <frederik.de.gottal@gmail.com>
2025-11-13 17:25:46 +0100

    Add id to buttons

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 let num1 = 0;
 let num2 = 0;
 let operator;
-//let result;
+let display;
+
+function getInput() {
+
+};
+
+function setDisplay() {
+    
+};
 
 function add(a, b) {
     return a + b;
@@ -22,7 +30,3 @@ function divide(a, b) {
 function operate(a, b, callback) {
     return callback(a, b);
 }
-
-//result = multiply(9,3);
-
-//console.log(result);
