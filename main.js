260de6fb1246d2c8d5ffbbc97d90ef2db6dc14f2
Fdg1994 <frederik.de.gottal@gmail.com>
2025-11-13 16:58:45 +0100

    Add operate function with callback to main.js

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 let num1 = 0;
 let num2 = 0;
 let operator;
+//let result;
 
-function add(a,b) {
+function add(a, b) {
     return a + b;
 };
 
-function subtract(a,b) {
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 };
 
-function multiply(a,b) {
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 };
 
-function divide(a,b) {
-    return a/b;
+function divide(a, b) {
+    return a / b;
 };
+
+function operate(a, b, callback) {
+    return callback(a, b);
+}
+
+//result = multiply(9,3);
+
+//console.log(result);
