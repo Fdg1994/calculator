a8d8e1324e5b31dbd5aa7ac2904b63388d7b31b8
Fdg1994 <frederik.de.gottal@gmail.com>
2025-11-18 13:52:03 +0100

    Change buttons from id to class in html

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let num1 = 0;
 let num2 = 0;
 let operator;
-let display;
+const display = 0;
 
 function getInput() {
 
 };
 
-function setDisplay() {
-    
+function updateDisplay() {
+
 };
 
 function add(a, b) {
