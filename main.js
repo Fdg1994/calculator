80cd596f00ed4552a9d29b9775ec1eb45a0229df
Fdg1994 <frederik.de.gottal@gmail.com>
2025-11-18 14:13:53 +0100

    Add event listener to number buttons

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let num1 = 0;
-let num2 = 0;
-let operator;
+const numberButtons = document.querySelectorAll('.btn-num');
+const operatorButtons = document.querySelectorAll('.btn-op');
+const equalsButton = document.getElementById('equals');
+const clearButton = document.getElementById('clear');
 const display = 0;
 
 function getInput() {
@@ -30,3 +31,9 @@ function divide(a, b) {
 function operate(a, b, callback) {
     return callback(a, b);
 }
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log("Number clicked:", button.textContent);
+  });
+});
